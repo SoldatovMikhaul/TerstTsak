4750090ca041aa65854a821ecfa3a274b2dca75d
SoldatovMikhaul <misha.soldatov.96@mail.ru>
2021-10-18 22:00:27 +0300

    add chandes to listbox

--- TestTSaskwMVVM/ParametrList.xaml.cs
+++ TestTSaskwMVVM/ParametrList.xaml.cs
@@ -23,5 +23,36 @@ namespace TestTakMVVM
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            List.Items.Add("Value");
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)//remove button
+        {
+            List.Items.Remove(List.SelectedItem);
+           // List.SelectedItems.IsReadOnly=false
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)//eddit
+        {
+            /*if (List.SelectedIndex < 0) return;
+
+            var tmpValue = List.Items[List.SelectedIndex].ToString();
+            RoomDisplayForm newRoomDisplayForm = new RoomDisplayForm();
+            newRoomDisplayForm.value = tmpValue;
+            newRoomDisplayForm.ShowDialog();
+
+            //TODO: inside "newRoomDisplayForm" set the value to the textbox
+            // ie.: myValueTextBox.Text = this.value;
+
+            if (newRoomDisplayForm.DialogResult == DialogResult.OK)
+            {
+                // replace the selected item with the new value
+                List.Items[List.SelectedIndex] = newRoomDisplayForm.value;
+            }*/
+            
+        }
     }
 }
